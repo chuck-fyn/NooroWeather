022b0ff0ecf5486967c1a6038e56c7e1401a9b18
Charlie Prutting <cprutting@gmail.com>
2024-12-14 11:05:22 -0800

    Initial Commit

---
+++ NooroWeather/NooroWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  NooroWeatherApp.swift
+//  NooroWeather
+//
+//  Created by Charles Prutting on 12/14/24.
+//
+
+import SwiftUI
+
+@main
+struct NooroWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
